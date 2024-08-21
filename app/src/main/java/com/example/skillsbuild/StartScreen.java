d1bad8a92a772d1653294fc6eaf2de8468b8264e
ejml1 <liethanjm@gmail.com>
2024-08-21 13:00:10 +0800

    Start screen button transfers to new page

--- app/src/main/java/com/example/skillsbuild/StartScreen.java
+++ app/src/main/java/com/example/skillsbuild/StartScreen.java
@@ -1,6 +1,9 @@
 package com.example.skillsbuild;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class StartScreen extends AppCompatActivity {
+
+    private Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class StartScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        continueButton = findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
